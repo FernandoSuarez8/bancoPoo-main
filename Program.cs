3c84df0f3cdb497933f38352ee20cc125bf8c543
fernandosuarez8 <dsuarez53@estudiantes.areandina.edu.co>
2022-09-15 18:18:59 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Iimplementacion de un cliente en el banco
+using bancoPoo.Models;
+
+Banco cliente = new Banco("123", "12023", "Carlos Moreno", "Pte de Cabi", 250000);
+Console.WriteLine("saldo Inicial del cliente : "+cliente.saldo);
+//realizare un deposito
+cliente.Depositar(75000);
+
+Console.WriteLine("saldo una vez depositado : "+cliente.saldo);
+
+//realizare un retiro
+cliente.retirar(35000);
+
+Console.WriteLine("saldo una vez retirado : "+cliente.saldo);
+
+
+//cosultare la cuenta
+Console.WriteLine("saldo final una vez depositado y retirado : "+cliente.consultaSaldo());
